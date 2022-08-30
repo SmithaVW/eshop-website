cb0a19d92a3eaf96f6be7fd9f1e8ecb38a9320c3
SmithaVW <smivw36@gmail.com>
2022-08-30 20:34:07 +0530

    Home Page completed

--- src/Home.js
+++ src/Home.js
@@ -13,16 +13,56 @@ function Home(){
         className="home__image" />
 
         <div className="home__row">
-            <Product />
-            <Product />
+            <Product 
+            id="12321341"
+            title=" Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling
+            Office Bag . Water Repellent Fabric for Men and Women (Blue). "
+            price={11.96}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+            />
+            
+            <Product
+            id="49538094"
+            title=" IFB 30 L Convection Microwave Oven (30BRC2, Black, With Starter kit)."
+            price={239.0}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/81D8pNFmWzL._SL1500_.jpg"
+             />
         </div>
         <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
+            <Product
+            id="4903850"
+            title="All the Light We Cannot See: The Breathtaking Worldwide Best Seller Paperback "
+            price={199.99}
+            rating={3}
+            image="https://images-eu.ssl-images-amazon.com/images/I/514kNkerQ0L._SY264_BO1,204,203,200_QL40.FMwebp_.jpg"
+            />
+            <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart Speaker with Alexa,Charcoal fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+             />
+            <Product
+            id="32544345"
+            title="New Apple iPad Pro(12.9-inch,Wi-Fi,128GB) - Silver(4th Generation)"
+            price={598.99}
+            rating={5}
+            image="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-ipad-pro-12-wifi-silver-2020?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1626721066000" 
+             />
+
         </div>
         <div className="home__row">
-            <Product />
+            <Product
+            id="23435680"
+            title="Samsung LC49RG90SSUXEN 49' curved LED Gaming Monitor - Super Ultra Wide Dual 
+            WQHD 5120x1440 "
+            price={1094.98}
+            rating={4}
+            image="https://images-eu.ssl-images-amazon.com/images/I/81rus0UFhsL.__AC_SY300_SX300_QL70_ML2_.jpg"
+             />
         </div>
 
 
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,29 @@
 import React from "react"
 import "./Product.css"
 
-function Product(){
+function Product({id,title,image,price,rating}){
     return(
-    <div>
-        <div className="product">
+    <div className="product">
         <div className="product__info">
-        <p> Bennett Mystic 15.6 inch Laptop Shoulder Messenger 
-            Office Bag.Water Repellent Fabric for Men and Women(Blue). </p>
+        <p> {title} </p>
         <p className="product__price"> 
         <small> $ </small>
-        <strong> 30 </strong>
+        <strong> {price} </strong>
         </p>
-        <div className="product__rating">⭐⭐⭐⭐</div>
-        </div>      
-        <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="product-pic"/>
-        <button> Add to Basket </button> 
-        
+        <div className="product__rating">
+        {Array(rating)
+            .fill()
+            .map((_,i) => (
+            <p>⭐</p>
+        ))}
         </div>
+        </div>      
+        <img src={image}/>
+
+        <button> Add to Basket </button>
+        
     </div>
+    
 )
 }
 
